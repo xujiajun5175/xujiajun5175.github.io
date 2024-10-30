4aec4bbbd38011a527112829088a0c094a03cd1e
徐家俊 <15151832830@163.com>
2024-10-30 14:27:42 +0800

    github action && github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 /*
  * @Author: 徐家俊 15151832830@163.com
  * @Date: 2024-10-28 09:17:05
- * @LastEditTime: 2024-10-29 23:17:29
- * @LastEditors: xujiajun xujiajun@vchangyi.com
+ * @LastEditTime: 2024-10-30 14:23:13
+ * @LastEditors: 徐家俊 15151832830@163.com
  * @Description:
- * @FilePath: \my-astro\astro.config.mjs
+ * @FilePath: /xujiajun.github.io/astro.config.mjs
  */
 // @ts-check
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
 import starlight from '@astrojs/starlight'
-import mdx from '@astrojs/mdx';
+import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS(), vue(), starlight({
-    title: '我的令人愉悦的文档网站'
-  }), mdx()]
+  site: 'https://xujiajun5175.github.io/',
+  integrations: [
+    UnoCSS(),
+    vue(),
+    starlight({
+      title: '我的令人愉悦的文档网站'
+    }),
+    mdx()
+  ]
 })
